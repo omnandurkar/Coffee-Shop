41106db5be92751eb8955f8b24b69cb7a7bdcdfd
V-dhumal <dhumalvaishnavi9356@gmail.com>
2024-02-22 18:11:10 +0530

    commit

---
+++ src/components/cards/Review.cards.js
@@ -0,0 +1,28 @@
+import React from "react";
+import '../cards/Review_cards.css'
+
+
+function Cards({ title, content, img }) {
+  return (
+    <div className="card-body">
+      <div className="card">
+        
+        
+          <p className="card-text ">
+            "{content}"</p>
+          <div className="p-1 rounded bg-cream  ">
+          <img
+            src={img} className="profile-image"
+          ></img>
+        </div>
+          
+          <p className=" title card-text-bg-danger">-{title}</p>
+            {/* <small class="text-body-secondary">9 mins</small> */}
+          </div>
+        </div>
+      
+   
+  );
+}
+
+export default Cards;
