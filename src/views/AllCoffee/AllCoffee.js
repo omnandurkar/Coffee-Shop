b84e1ca8291aa0758499c14e0d0f662092244f8f
Sunnyk16 <kumarsunny1246@gmail.com>
2024-02-22 15:24:10 +0530

    ref:cards added

--- src/views/AllCoffee/AllCoffee.js
+++ src/views/AllCoffee/AllCoffee.js
@@ -2,25 +2,28 @@ import React from 'react'
 import Cards from '../../components/cards/Cards'
 import config from '../../config/coffee.json'
 
+
 function AllCoffee() {
   return (
-    <div>
-      <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
-      <div className='col'> 
+    <div className='bg-cream  '>
+      <h2 className='text-center fs-24 mx-2'>Menu</h2>
+      <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3 mx-2 ">
+      
       {
         config.map((coffeedata,index)=>{
           const {img,title,content}=coffeedata
           return(
+            <div className='col'> 
             <Cards img={img} title={title} content={content} />
+            </div>
 
-  
           )
   
         })
       }
 
       
-      </div>
+      
 
       </div>
     </div>
