1880682065f0982dc1a337a116cdba7116c03398
sanketbhure <sanketbhure1306@gmail.com>
2024-02-23 10:05:49 +0530

    header img added

--- src/views/Subscriptions/Subscriptions.js
+++ src/views/Subscriptions/Subscriptions.js
@@ -1,12 +1,16 @@
 import React from 'react'
 import "./Subscriptions.css"
 import Navbar from '../../components/Navbar/Navbar'
+import Subimg1 from "./subscribe-and-save.webp"
 
 function Subscriptions() {
   return (
     <>
       <Navbar />
-      <div>
+      <div className='sub-header-container'>
+        <div>
+          <img src={Subimg1} alt='' className='sub-img1' />
+        </div>
         Subscriptions
       </div>
     </>
