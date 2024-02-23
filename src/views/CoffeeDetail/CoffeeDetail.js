ba85c67eba4e77c98131cc1f9533f66d1234c056
Sadanand09 <sadanandgurude9@gmail.com>
2024-02-24 00:41:32 +0530

    ref: changes added

--- src/views/CoffeeDetail/CoffeeDetail.js
+++ src/views/CoffeeDetail/CoffeeDetail.js
@@ -1,11 +1,35 @@
 import React from 'react'
+import Navbar from '../../components/Navbar/Navbar'
+import config from "../../config/Detail.json";
 
 function CoffeeDetail() {
-  return (
-    <div>
-      CoffeeDetail
-    </div>
-  )
+
+    {
+        config.map((coffeedata, index) => {
+        const {img} = coffeedata;
+    
+    })}
+    return (
+        <>
+            <Navbar />
+
+            <div className='sub-header-container d-flex'>
+
+                <div className=''>
+
+                </div>
+                <div>
+                    
+                        return (
+                            <div className="col">
+                                 <img src={img} />
+                            </div>
+                        );
+                    
+                </div>
+            </div>
+        </>
+    )
 }
 
 export default CoffeeDetail
