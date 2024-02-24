f3b0b949d995896dd89a1bc925f44ff89c6564a7
V-dhumal <dhumalvaishnavi9356@gmail.com>
2024-02-24 14:04:23 +0530

    commit

--- src/views/Review/Review.js
+++ src/views/Review/Review.js
@@ -16,7 +16,7 @@ function Review() {
       <p className='title'>Your Words Matter</p>
       <p className='description'>Share your experiences and feedback to help us brew excellence at Our Coffee Shop .<br/>
        Your voice guides our journey towards perfection.</p>
-      <img src="https://th.bing.com/th/id/R.3c67e53d682589dcb8ce02dd3ac1e20c?rik=LeUqj%2bnBd3%2f0WA&riu=http%3a%2f%2fwallup.net%2fwp-content%2fuploads%2f2017%2f11%2f17%2f239445-coffee-coffee_beans-cup.jpg&ehk=%2bEd%2bhMjaHGMrExklwM9MNbALfkaDNqvDmS67gs%2bf2OA%3d&risl=&pid=ImgRaw&r=0" className='head-img '></img>
+      <img src="https://th.bing.com/th/id/R.3c67e53d682589dcb8ce02dd3ac1e20c?rik=LeUqj%2bnBd3%2f0WA&riu=http%3a%2f%2fwallup.net%2fwp-content%2fuploads%2f2017%2f11%2f17%2f239445-coffee-coffee_beans-cup.jpg&ehk=%2bEd%2bhMjaHGMrExklwM9MNbALfkaDNqvDmS67gs%2bf2OA%3d&risl=&pid=ImgRaw&r=0" className='head-img img-fluid '></img>
       <h1 className='heading-1'>Featured Reviews</h1>
       <br/> <br/>
       <div className='content-1'>
