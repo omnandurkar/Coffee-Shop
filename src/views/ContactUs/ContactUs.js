67aa638fc32f3a253b8d4fc91a32d83bf91fa44f
Sadanand09 <sadanandgurude9@gmail.com>
2024-02-24 18:10:55 +0530

    op

--- src/views/ContactUs/ContactUs.js
+++ src/views/ContactUs/ContactUs.js
@@ -5,6 +5,7 @@ import twitter from "./twitter.png";
 import youtube from "./youtube.png";
 import Navbar from '../../components/Navbar/Navbar';
 import "./Contact.css";
+import Footer from '../../components/Footer/Footer';
 
 function ContactUs() {
   return (
@@ -14,7 +15,7 @@ function ContactUs() {
         <h6>
           Contact us
         </h6>
-        <h1>
+        <h1 className='g-font-b'>
           Coffee Shop
         </h1>
       </div>
@@ -25,7 +26,7 @@ function ContactUs() {
           <img src={shop} className="shop img-fluid" />
         </div>
 
-        <div className=''>
+        <div className='my-auto ps-3 '>
 
           <div>
             <p>
@@ -44,6 +45,21 @@ function ContactUs() {
         </div>
 
       </div>
+      <div className='text-center mt-4 p-5'>
+        <h1 className='g-font-b'>Won the best coffee award 2023</h1>
+
+        <p className=''>"At Coffee Shop, we're honored to be recognized as the home of the best coffee of 2023. This prestigious award is a testament to our dedication to crafting exceptional coffee experiences for our customers."</p>
+
+        <p>
+        From our carefully sourced beans to our expertly trained baristas, every cup of coffee at Coffee Shop is crafted with passion and precision. Winning the award for the best coffee of 2023 reaffirms our commitment to excellence in every sip.
+        </p>
+
+        <p>
+        Being named the best coffee of 2023 is more than just an accolade for us at Coffee Shop; it's a validation of our relentless pursuit of quality and flavor. We take pride in serving not just a beverage, but an experience that delights the senses and warms the soul.
+        </p>
+      </div>
+
+      <Footer/>
     </div>
   )
 }
