d9683f7b4c3e35228c01039c078018f0a77d51f6
Sadanand09 <sadanandgurude9@gmail.com>
2024-02-24 09:23:03 +0530

    commit added

--- src/components/cards/Cards.js
+++ src/components/cards/Cards.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Link } from 'react-router-dom';
 import "../cards/Cards.css";
 
-function Cards({id, title, content, img }) {
+function Cards({ id, title, content, img}) {
   return (
     <>
       <div className=" rounded-1  shadow-lg m-1 p-1 w-100  bg-light hover ">
@@ -15,14 +15,16 @@ function Cards({id, title, content, img }) {
 
         <div className=" p-2">
           <p className=" fw-bold  fs-4">{title}</p>
-          <p className=" p-2 fs-7">{content.substring(0 ,200  )}{content.length > 200? '...' :''}</p>
+          <p className=" p-2 fs-7">{content.substring(0, 200)}{content.length > 200 ? '...' : ''}</p>
           <div className="d-flex justify-content-between align-items-center">
             <div className="btn-group">
               <span class="btn btn-sm btn-outline-secondary">
-              <Link to={`/menu/${id}`}>View More</Link>
+                <Link to={`/menu/${id}`}>
+                  View More
+                </Link>
               </span>
             </div>
-         </div>
+          </div>
         </div>
       </div>
     </>
--- src/views/CoffeeDetail/CoffeeDetail.js
+++ src/views/CoffeeDetail/CoffeeDetail.js
@@ -1,38 +1,96 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
 import Navbar from '../../components/Navbar/Navbar'
-import config from "../../config/Detail.json";
-import img from "./Espresso_0.avif"
+import config from "../../config/coffee.json";
+
 
 function CoffeeDetail() {
 
-    {
-        config.map((coffeedata, index) => {
-            const { img } = coffeedata;
+    const { id } = useParams()
 
+    const [coffeedata, setCoffeedata] = useState({})
+
+    useEffect(() => {
+        config.forEach((infoObj) => {
+            if (infoObj.id == id) {
+                setCoffeedata(infoObj)
+            }
         })
+    }, [id])
+
+    const increaseQuantity= () => {
+        const quantityElement = document.getElementById("quantity");
+        let quantity = parseInt(quantityElement.innerText);
+        quantityElement.innerText = ++quantity;
     }
+
+
+    const decreaseQuantity=() => {
+        const quantityElement = document.getElementById("quantity");
+        let quantity = parseInt(quantityElement.innerText);
+        quantityElement.innerText = --quantity;
+    }
+
+
     return (
         <>
             <Navbar />
 
-            <div className='sub-header-container d-flex flex-row justify-content-around '>
+            <div className='sub-header-container d-flex flex-row justify-content-center'>
 
-                <div className=''>
-
-                    <h1>Here is your clas</h1>
-
+                <div className='me-4'>
+                    <img src={coffeedata.img} className='h-auto' />
                 </div>
 
                 <div>
-
                     <div className="">
-                        <img className='h-4' src={img} />
-                    </div>
+                        <h1 className=''>
+                            {coffeedata.title}
+                        </h1>
+                        <b className='text-danger '>
+                            {coffeedata.price}
+                        </b>
 
+                        <div className='d-flex flex-row  '>
+                            <div className='d-flex flex-row justify-content-around '>
+                                <p onClick={decreaseQuantity} >
+                                    -
+                                </p>
+
+                                <span id='quantity'>
+                                    1
+                                </span>
+
+                                <p onClick={increaseQuantity}>
+                                    +
+                                </p>
+
+                            </div>
+
+                            <div>
+                                <button type='button' className='btn bg-danger text-white  '>
+                                    Buy now
+                                </button>
+
+                            </div>
+                        </div>
+                        <div className='d-flex'>
+                            <b className='font-bold'>Category: </b>
+                            <p className='fs-6'> {coffeedata.category}</p>
+                        </div>
+
+                        <div className='d-flex'>
+                            <b className='font-bold'>Product Id: </b>
+                            <p className='fs-6'> {coffeedata.pId}</p>
+                        </div>
+                    </div>
                 </div>
+
             </div>
         </>
     )
+
+
 }
 
 export default CoffeeDetail
